de859f4e5b6ed558957d64749920c78ec8ebfc54
zidanekinnoume-47bit <zidanekinnoume47@gmail.com>
2026-08-02 11:38:32 +0200

    Correction connexion MySQL et affichage des tournois

--- config/database.js
+++ config/database.js
@@ -3,35 +3,26 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-
     host: process.env.MYSQLHOST,
-
     user: process.env.MYSQLUSER,
-
     password: process.env.MYSQLPASSWORD,
-
     database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
 
-    port: process.env.MYSQLPORT
-
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
+db.getConnection((err, connection) => {
 
-db.getConnection((err, connection)=>{
-
-    if(err){
-
+    if (err) {
         console.log("Erreur connexion base :", err);
-
-    }else{
-
+    } else {
         console.log("Base de données connectée");
-
         connection.release();
-
     }
 
 });
-
 
 module.exports = db;
